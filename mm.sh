42f49de803af305b8d6cc68fa3f8cbc1ca57634f
Larry Liang <ptolemy428@gmail.com>
2014-11-18 21:29:25 +1100

    updates

--- mm.sh
+++ mm.sh
@@ -2,11 +2,7 @@
 
 dev_root=/home/larry/development
 
-if [ "$1" == "8" ]; then
-	export JAVA_HOME=$dev_root/applications/java/jdk1.8.0_25
-else
-	export JAVA_HOME=$dev_root/applications/java/jdk1.7.0_71
-fi
+export JAVA_HOME=$dev_root/applications/java/jdk1.7.0_71
 
 
 export SCALA_HOME=$dev_root/applications/scala/scala-2.11.2
@@ -15,6 +11,8 @@ export GROOVY_HOME=$dev_root/applications/groovy/groovy-2.3.0
 export GRADLE_HOME=$dev_root/applications/gradle/gradle-2.1
 export PLAY_HOME=$dev_root/applications/play/activator-1.2.10-minimal
 export M2_HOME=$dev_root/applications/maven/apache-maven-3.2.3
+
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
 
 PATH=$PATH:$JAVA_HOME/bin:$IDEA_HOME/bin:$M2_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin:$SBT_HOME/bin:$GROOVY_HOME/bin:$PLAY_HOME:$M2_HOME/bin
 export PATH
@@ -26,12 +24,13 @@ alias dev='cd $dev_root/'
 alias apps='cd $dev_root/applications/'
 alias i='idea.sh &'
 alias sj='cd $dev_root/playpen/play2'
-alias vmup='VBoxManage startvm dev'
-alias vmdown='VBoxManage controlvm dev poweroff'
+alias mm='cd $dev_root/git-mm'
+alias aws='~/bin/aws'
+alias vmup='VBoxManage startvm MessageMediaDev'
+alias vmdown='VBoxManage controlvm MessageMediaDev poweroff'
 
+#set up aws env for mm
+. $dev_root/aws/mm/aws-mm.sh
 
-#set up aws env
-. $dev_root/aws/c2014/aws-c2014.sh
-
-#intellij work-around
+#intellij in work-around
 . $dev_root/dev-misc/scripts/idea-lin.sh
