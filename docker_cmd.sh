63061160a436900e8cbf503cefdc5210196760e5
Larry Liang <ptolemy428@gmail.com>
2014-12-24 14:10:31 +1100

    update docker commands

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -2,9 +2,9 @@ alias sudo='sudo '
 alias redis-server='docker run -d --name redis -p 6379:6379 dockerfile/redis'
 alias redis-cli="docker run -it --rm --link redis:redis dockerfile/redis bash -c 'redis-cli -h redis'"
 
-alias rabbitmq-server='docker run -d -p 5672:5672 -p 15672:15672 dockerfile/rabbitmq'
+alias rabbitmq-server='docker run -d --name rabbitmq -p 5672:5672 -p 15672:15672 dockerfile/rabbitmq'
 
-alias dynamo-server='docker run -d -p 8000:8000 ptolemy428/docker-dynamodb-local'
+alias dynamo-server='docker run -d --name dynamo -p 7999:7999 ptolemy428/docker-dynamodb-local'
 
 alias rm-containers='sudo docker rm $(sudo docker ps -a -q)'
 
